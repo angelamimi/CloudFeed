592efd1f100a93abef003572697cccc66e2f2964
Angela <1842841+angelamimi@users.noreply.github.com>
2025-05-30 16:02:55 -0500

    Video controls update

--- CloudFeed/Viewer/ControlsView.swift
+++ CloudFeed/Viewer/ControlsView.swift
@@ -71,6 +71,10 @@ class ControlsView: UIView {
         commonInit()
     }
     
+    deinit {
+        NotificationCenter.default.removeObserver(self, name: UIApplication.willEnterForegroundNotification, object: nil)
+    }
+    
     private func commonInit() {
         
         guard let view = loadViewFromNib() else { return }
@@ -79,6 +83,10 @@ class ControlsView: UIView {
         
         addSubview(view)
         
+        initObservers()
+        
+        setVolumePosition()
+        
         setPlaying(playing: false)
         initControls()
     }
@@ -86,6 +94,27 @@ class ControlsView: UIView {
     private func loadViewFromNib() -> UIView? {
         let nib = UINib(nibName: "ControlsView", bundle: nil)
         return nib.instantiate(withOwner: self, options: nil).first as? UIView
+    }
+    
+    private func initObservers() {
+        NotificationCenter.default.addObserver(forName: UIApplication.willEnterForegroundNotification, object: nil, queue: nil) { [weak self] _ in
+            DispatchQueue.main.async { [weak self] in
+                self?.willEnterForegroundNotification()
+            }
+        }
+    }
+    
+    private func willEnterForegroundNotification() {
+        setVolumePosition()
+    }
+    
+    private func setVolumePosition() {
+        let contentSize = UIApplication.shared.preferredContentSizeCategory
+        if contentSize > .accessibilityLarge {
+            volumeTopConstraint.constant = 88
+        } else {
+            volumeTopConstraint.constant = 58
+        }
     }
     
     func reset() {
